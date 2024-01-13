e324d6bfef418cf8a771608ee037b3e8e19f4b9e
RiaanSadiq <136353230+RiaanSadiq@users.noreply.github.com>
2024-01-13 19:45:47 +0500

    removing errors

--- src/components/widgets/Outcome.tsx
+++ src/components/widgets/Outcome.tsx
@@ -15,7 +15,7 @@ const Outcome = () => {
           {/* right-Content */}
           <div className="flex-1">
            <h2 className="text-4xl font-bold  ">The Outcome for Participants of the Program</h2> 
-           <p className="text-slate-600 mt-6 text-lg">As a graduate of this program, you will own valuable products such as Full-Stack App Templates, AR and VR Experiences, and APIs that are marketed globally by the Panaverse DAO. You will also have the opportunity to offer your services at a rate of $50 per hour, providing a path to financial stability while contributing to the growth of Pakistan's software exports.</p>
+           <p className="text-slate-600 mt-6 text-lg">As a graduate of this program, you will own valuable products such as Full-Stack App Templates, AR and VR Experiences, and APIs that are marketed globally by the Panaverse DAO. You will also have the opportunity to offer your services at a rate of $50 per hour, providing a path to financial stability while contributing to the growth of Pakistans software exports.</p>
           <div className="mt-6 mb-2 grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-8">
            { 
            outComePoints.map((item,i)=>(
